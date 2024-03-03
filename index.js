e8b03c32491a9cc139723d1808247af09fc91c5a
solarnius <solarnius@sagods.com>
2024-03-02 18:45:05 -0800

    add buffer polyfill

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /**
  * @format
  */
-
+import 'react-native-get-random-values';
+import {Buffer} from 'buffer';
+global.Buffer = Buffer;
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
